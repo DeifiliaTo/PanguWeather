aee3e550190ecf7b289b32f2b9abd9a6911ef102
Deifilia To <deifilia.to@gmail.com>
2024-03-27 13:44:54 +0100

    weighting and cosine anealing

--- train.py
+++ train.py
@@ -7,7 +7,6 @@ from utils.data_loader_multifiles import get_data_loader
 import torch
 from torch.cuda.amp import autocast, GradScaler
 import torch.distributed as dist
-from utils.data_loader_multifiles import get_data_loader
 from torch.nn.parallel import DistributedDataParallel as DDP
 import os
 import utils.eval as eval
@@ -45,12 +44,10 @@ def init_distributed(params):
     return device, slurm_localid, gpus_per_node, rank, world_size
 
 def training_loop(params, device, slurm_localid, gpus_per_node):
-    print("model dir", params['save_dir'][params['model']])
-    
     # Define patch size, data loader, model
     C          = params['C']
-    train_data_loader, train_dataset, train_sampler = get_data_loader(params, params['train_data_path'], dist.is_initialized(), train=True, device=device, patch_size=params['patch_size'], subset_size=params['subset_size'])
-    valid_data_loader, valid_dataset = get_data_loader(params, params['valid_data_path'], dist.is_initialized(), train=False, device=device, patch_size=params['patch_size'], subset_size=params['validation_subset_size'])
+    train_data_loader, train_dataset, train_sampler = get_data_loader(params, params['train_data_path'], dist.is_initialized(), mode='train', device=device, patch_size=params['patch_size'], subset_size=params['subset_size'])
+    valid_data_loader, valid_dataset = get_data_loader(params, params['valid_data_path'], dist.is_initialized(), mode='validation', device=device, patch_size=params['patch_size'], subset_size=params['validation_subset_size'])
 
     if params['model'] == 'pangu':
         model = PanguModel(device=device, C=C, patch_size=params['patch_size'])
@@ -75,10 +72,10 @@ def training_loop(params, device, slurm_localid, gpus_per_node):
                 output_device=slurm_localid,
                 find_unused_parameters=False
         )
-
     
     optimizer = torch.optim.Adam(model.parameters(), lr=5e-4, weight_decay=3e-6)
-    scaler = GradScaler()
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, params['num_epochs'])
+    #scaler = GradScaler()
     start = time.perf_counter() # Measure training time.
 
     if dist.is_initialized():
@@ -90,7 +87,7 @@ def training_loop(params, device, slurm_localid, gpus_per_node):
 
     if params['restart']:
         print("params['save_dir'][params['model']]", params['save_dir'][params['model']])
-        state = torch.load(params['save_dir'][params['model']] + str(params['save_counter']) + '_' + params['model'] + params['hash'] + '.pt', map_location=f'cuda:{rank}')
+        state = torch.load(params['save_dir'][params['model']] + str(params['save_counter']) + '_' + params['model'] + params['hash'] + '.pt')#, map_location=f'cuda:{rank}')
         model.load_state_dict(state['model_state'])
         optimizer.load_state_dict(state['optimizer_state'])
 
@@ -107,6 +104,10 @@ def training_loop(params, device, slurm_localid, gpus_per_node):
     else:
         save_counter = 0
 
+    # Z, Q, T, U, V
+    pressure_weights = torch.tensor([3.00, 0.6, 1.5, 0.77, 0.54]).view(1, 5, 1, 1, 1).to(device)
+    surface_weights  = torch.tensor([1.5, 0.77, 0.66, 3.0]).view(1, 4, 1, 1).to(device)
+
     for epoch in range(params['num_epochs']):
         start_epoch_time = time.perf_counter()
         model.train()
@@ -114,28 +115,31 @@ def training_loop(params, device, slurm_localid, gpus_per_node):
         train_sampler.set_epoch(epoch)
         epoch_average_loss = 0
         for i, data in enumerate(train_data_loader):        # Load weather data at time t as the input; load weather data at time t+1/3/6/24 as the output
-            optimizer.zero_grad()
-            input, input_surface, target, target_surface = data[0], data[1], data[2], data[3]
+            input, input_surface, target, target_surface = data[0], data[1], data[2][0], data[3][0]
             input = input.to(torch.float32).to(device)
             input_surface = input_surface.to(torch.float32).to(device)
             target = target.to(torch.float32).to(device)
             target_surface = target_surface.to(torch.float32).to(device)
-            with autocast():
-                # Call the model and get the output
-                output, output_surface = model(input, input_surface)
+            
+            # Call the model and get the output
+            output, output_surface = model(input, input_surface)
 
-                # We use the MAE loss to train the model
-                # The weight of surface loss is 0.25
-                # Different weight can be applied for different fields if needed
-                loss = 1 * loss1(output, target) + 0.25 * loss2(output_surface, target_surface)
-                # Call the backward algorithm and calculate the gradient of parameters
+            # We use the MAE loss to train the model
+            # The weight of surface loss is 0.25
+            # Different weight can be applied for different fields if needed
+            output, output_surface = output * pressure_weights, output_surface * surface_weights
+            target, target_surface = target * pressure_weights, target_surface * surface_weights
+            loss = 1 * loss1(output, target) + 0.25 * loss2(output_surface, target_surface)
+            # Call the backward algorithm and calcula the gradient of parameters
             torch.cuda.empty_cache()
-            scaler.scale(loss).backward()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
                 
             # Update model parameters with Adam optimizer
             # The learning rate is 5e-4 as in the paper, while the weight decay is 3e-6
-            scaler.step(optimizer)
-            scaler.update()
+            #scaler.step(optimizer)
+            #scaler.update()
             #print(optimizer.param_groups)
             
             dist.all_reduce(loss) # Allreduce rank-local mini-batch losses.
@@ -148,7 +152,7 @@ def training_loop(params, device, slurm_localid, gpus_per_node):
                 print(f'Epoch: {epoch+1:03d}/{num_epochs:03d} '
                     f'| Batch {i:04d}/{len(train_data_loader):04d} '
                     f'| Averaged Loss: {loss:.4f}')
-                
+        scheduler.step()
         epoch_average_loss /= len(train_data_loader)
         
         end_epoch_time = time.perf_counter()
@@ -160,12 +164,12 @@ def training_loop(params, device, slurm_localid, gpus_per_node):
         model.eval()
         with torch.no_grad():
             # Get rank-local numbers of correctly classified and overall samples in training and validation set.
-            val_loss, MSE, acc, total_samples, dt_validation = eval.get_loss(model, valid_data_loader, device, loss1, loss2, lat_crop=params['lat_crop'], lon_crop=params['lon_crop'])
+            val_loss, MSE, acc, total_samples, dt_validation = eval.get_loss(model, valid_data_loader, device, loss1, loss2, lat_crop=params['lat_crop'], lon_crop=params['lon_crop'], world_size=world_size)
 
             if rank == 0:
-                valid_loss_history.append(val_loss.item())
-                if (val_loss < best_val_loss) or (epoch % 20 == 0):
-                    best_val_loss = val_loss
+                valid_loss_history.append(val_loss[0])
+                if (val_loss[0] < best_val_loss) or (epoch % 20 == 0):
+                    best_val_loss = val_loss[0]
                     # save model to path
                     save_path = params['save_dir'][params['model']] + str(save_counter) + "_" + params['model'] + params['hash'] + '.pt'
                     state = {
@@ -177,17 +181,17 @@ def training_loop(params, device, slurm_localid, gpus_per_node):
                 
                 print(f'Epoch Average: {int(epoch)+1:03d}/{int(num_epochs):03d} ')
                 print(f'| Mean L1 Training Loss: {epoch_average_loss :.3f} ')
-                print(f'| Mean L1 Validation Loss: {val_loss :.3f} ')
-                print(f'| MSE T500: {MSE[0] :.3f} ')
-                print(f'| MSE Z500: {MSE[1] :.3f} ')
-                print(f'| MSE T2M:  {MSE[2] :.3f} ')
-                print(f'| MSE U10:  {MSE[3] :.3f} ')
-                print(f'| MSE V10:  {MSE[4] :.3f} ')
-                print(f'| ACC T500: {acc[0] :.3f} ')
-                print(f'| ACC Z500: {acc[1] :.3f} ')
-                print(f'| ACC T2M:  {acc[2] :.3f} ')
-                print(f'| ACC U10:  {acc[3] :.3f} ')
-                print(f'| ACC V10:  {acc[4] :.3f} ')
+                print(f'| Mean L1 Validation Loss: {val_loss[0] :.3f} ')
+                print(f'| MSE T850: {MSE[0][0] :.3f} ')
+                print(f'| MSE Z500: {MSE[1][0] :.3f} ')
+                print(f'| MSE T2M:  {MSE[2][0] :.3f} ')
+                print(f'| MSE U10:  {MSE[3][0] :.3f} ')
+                print(f'| MSE V10:  {MSE[4][0] :.3f} ')
+                print(f'| ACC T850: {acc[0][0] :.3f} ')
+                print(f'| ACC Z500: {acc[1][0] :.3f} ')
+                print(f'| ACC T2M:  {acc[2][0] :.3f} ')
+                print(f'| ACC U10:  {acc[3][0] :.3f} ')
+                print(f'| ACC V10:  {acc[4][0] :.3f} ')
                 print(f'| Validation time: {dt_validation : .3f}')
                 print(f'| Total samples: {total_samples :.3f}')
                     
@@ -200,19 +204,20 @@ if __name__ == '__main__':
     params['valid_data_path'] =  '/lsdf/kit/imk-tro/projects/Gruppe_Quinting/ec.era5/1959-2023_01_10-wb13-6h-1440x721.zarr'
     params['pressure_static_data_path'] = '/hkfs/work/workspace/scratch/ke4365-pangu/PANGU_ERA5_data_v0/static/pressure_zarr.npy' 
     params['surface_static_data_path'] =  '/hkfs/work/workspace/scratch/ke4365-pangu/PANGU_ERA5_data_v0/static/surface_zarr.npy'  
-    params['dt'] = 1
+    params['dt'] = 24
     params['num_data_workers'] = 2
     params['data_distributed'] = True
     params['filetype'] = 'zarr' # hdf5, netcdf, or zarr
-    params['num_epochs'] = 100
+    params['num_epochs'] = 50
     num_epochs = params['num_epochs']
     params['C'] = 192
-    params['subset_size'] = None
-    params['validation_subset_size'] = None
+    params['subset_size'] = 4000
+    params['validation_subset_size'] = 500
     params['restart'] = False
-    params['hash'] = None # "20240319_" + str(338453693)
+    params['hash'] = None # "20240326_" + str(673944995)
     params['Lite'] = True
-    params['save_counter'] = None
+    params['daily'] = False
+    params['save_counter'] = None # 44
 
     # Specify model
     params['model'] = 'panguLite'
@@ -282,13 +287,13 @@ if __name__ == '__main__':
     # initialize patch size: currently, patch size is only (2, 8, 8) for PanguLite. PS is (2, 4, 4) for all other sizes.
     if params['Lite'] == True:
         params['patch_size'] = (2, 8, 8)
-        params['batch_size'] = 6
+        params['batch_size'] = 5
         params['lat_crop']   = (3, 4)
         params['lon_crop']   = (0, 0)
     else:
         params['patch_size'] = (2, 4, 4)
         params['batch_size'] = 2
-        params['lat_crop']   = (0, 1)
+        params['lat_crop']   = (1, 2)
         params['lon_crop']   = (0, 0)
 
     training_loop(params, device, slurm_localid, gpus_per_node)
