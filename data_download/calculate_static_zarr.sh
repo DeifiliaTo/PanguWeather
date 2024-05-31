940115936163007ec7b649fe26e299c6834c1ca9
Deifilia To <deifilia.to@gmail.com>
2024-05-31 11:03:12 +0200

    run scripts

--- data_download/calculate_static_zarr.sh
+++ data_download/calculate_static_zarr.sh
@@ -2,10 +2,11 @@
 #SBATCH --nodes=1
 #SBATCH --time=2:00:00
 #SBATCH --partition cpuonly
+#SBATCH --account hk-project-p0021348
 
 module purge
-module load jupyter/tensorflow
+module load jupyter/tensorflow eccodes-2.30.2_i22_ompi40
 source ~/py39/bin/activate
 which python
 
-python calculate_static_zarr.py
+python calculate_static_zarr_all.py
--- run.sh
+++ run.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-#SBATCH --partition=dev_accelerated
+#SBATCH --partition=accelerated
 #SBATCH --gres=gpu:4
-#SBATCH --time=1:00:00
-#SBATCH --nodes=1
+#SBATCH --time=12:00:00
+#SBATCH --nodes=16
 #SBATCH --ntasks-per-node=4
+#SBATCH --account='hk-project-p0021348'
+#SBATCH --output='PanguLite-batch64-time.out'
+#SBATCH --job-name='PGL64'
 
 module purge # Unload all models.
-module load jupyter/tensorflow
+module load jupyter/tensorflow eccodes-2.30.2_i22_ompi40
 
 source ~/py39/bin/activate
 which python
 
 # Change 5-digit MASTER_PORT as you wish, SLURM will raise Error if duplicated with others.
-export MASTER_PORT=12341
+export MASTER_PORT=12340
 
 # Get the first node name as master address.
 master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
